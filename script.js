af90454a55f62d8470602d660ac80a11bd95e99b
Juan <juanfuentesrufino6@gmail.com>
2024-03-29 15:03:32 -0300

    Primeiro exercício feito por Juan. Link github: https://github.com/JuanRufino63

---
+++ script.js
@@ -0,0 +1,9 @@
+//Exercício 1
+/*
+let nome; //variável com valor undefinied
+let trabalho = null; //variável com valor vazio
+let checkpoint = "cidade"; //variável com valor
+console.log(nome);//para sair no console
+console.log(trabalho);//para sair no console
+console.log(checkpoint);//para sair no console
+*/
