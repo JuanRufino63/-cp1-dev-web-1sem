66dc4c4ab33a3d0eae84a1466a08e584fed54550
Juan <juanfuentesrufino6@gmail.com>
2024-03-29 15:06:40 -0300

    Segundo exercício feito por Juan. Link github: https://github.com/JuanRufino63

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ console.log(nome);//para sair no console
 console.log(trabalho);//para sair no console
 console.log(checkpoint);//para sair no console
 */
+//Exercício 2
+
+/*
+//valores das variáveis
+let valor1 = 10; 
+let valor2 = 20;
+let valor3 = 30;
+
+//estruturas de decisão
+if (valor1 >= 8) //checa se valor1 maior igual a 8
+{
+    console.log("O valor é igual ou maior que 8"); //se for, apresenta no console a mensagem
+}
+if (valor2 != 25) //checa se valor2 é diferente de 25
+{
+    console.log("O valor é diferente de 25"); //se for, apresenta no console a mensagem
+}
+if (valor3 ==30) //checa se valor3 é igual a 30
+{
+    console.log("O valor é de fato igual a 30")//se for, apresenta no console a mensagem
+}
+*/
